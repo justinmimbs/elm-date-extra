aee8f16d3ee72d287271dac0ce7a9b57f649cfa6
Justin Mimbs <justin.mimbs@gmail.com>
2016-06-14 07:20:24 -0400

    Add tests for quarter extraction

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -3,7 +3,43 @@ module Tests exposing (..)
 import Date exposing (Date, Month(..))
 import DateFunctions exposing (..)
 
-import ElmTest exposing (Test, suite, test, defaultTest, equals, assertEqual, assertNotEqual, assertionList, runSuiteHtml)
+import ElmTest exposing (Test, suite, test, defaultTest, equals, assert, assertEqual, assertNotEqual, assertionList, runSuiteHtml)
+
+everyYMDInMonth : Int -> Month -> List (Int, Month, Int)
+everyYMDInMonth y m =
+  List.map (\d ->
+    (y, m, d)
+  ) [ 1 .. (daysInMonth y m) ]
+
+everyYMDInYear : Int -> List (Int, Month, Int)
+everyYMDInYear y =
+  List.concatMap (everyYMDInMonth y) months
+
+everyDateInMonth : Int -> Month -> List Date
+everyDateInMonth y m =
+  everyYMDInMonth y m |> List.map (\(y, m, d) -> dateFromYMD y m d)
+
+
+{-| Converting (year, month, day) : (Int, Month, Int) to RataDie and back is lossless.
+-}
+
+rataDieTests : Test
+rataDieTests =
+  let
+    ymdList : List (Int, Month, Int)
+    ymdList =
+      List.concatMap everyYMDInYear <| List.concat [ [ 1 .. 4 ], [ 97 .. 105 ], [ 397 .. 405], [ 1897 .. 1905 ], [ 1997 .. 2005 ] ]
+
+    ymdFromRataDieFromYMD : (Int, Month, Int) -> (Int, Month, Int)
+    ymdFromRataDieFromYMD (y, m, d) =
+      ymdFromRataDie <| rataDieFromYMD y m d
+  in
+    suite "RataDie" <|
+      List.map defaultTest <| assertionList ymdList <| List.map ymdFromRataDieFromYMD ymdList
+
+
+{-| isoYear, isoWeek, isoWeekday return expected results.
+-}
 
 isoWeekDateTests : Test
 isoWeekDateTests =
@@ -16,7 +52,7 @@ isoWeekDateTests =
     isoWeekDateFromYMD y m d =
       isoWeekDateFromDate <| dateFromYMD y m d
   in
-    suite "isoWeekYear, isoWeek, isoWeekday" [
+    suite "isoYear, isoWeek, isoWeekday" [
       equals (isoWeekDateFromYMD 2005 Jan  1) (2004, 53, 6),
       equals (isoWeekDateFromYMD 2005 Jan  2) (2004, 53, 7),
       equals (isoWeekDateFromYMD 2005 Dec 31) (2005, 52, 6),
@@ -35,35 +71,34 @@ isoWeekDateTests =
       equals (isoWeekDateFromYMD 2010 Jan  3) (2009, 53, 7)
     ]
 
--- RataDie losslessly encodes (Year, Month, Day)
+{-| quarter returns expected results.
+-}
 
-rataDieTests : Test
-rataDieTests =
+quarterTests : Test
+quarterTests =
   let
-    everyYMDInYear : Int -> List (Int, Month, Int)
-    everyYMDInYear y =
-      List.concatMap (\m ->
-        List.map (\d ->
-          (y, m, d)
-        ) [ 1 .. (daysInMonth y m) ]
-      ) months
-
-    ymdList : List (Int, Month, Int)
-    ymdList =
-      List.concatMap everyYMDInYear <| List.concat [ [ 1 .. 4 ], [ 97 .. 105 ], [ 397 .. 405], [ 1897 .. 1905 ], [ 1997 .. 2005 ] ]
-
-    ymdFromRataDieFromYMD : (Int, Month, Int) -> (Int, Month, Int)
-    ymdFromRataDieFromYMD (y, m, d) =
-      ymdFromRataDie <| rataDieFromYMD y m d
+    everyDateInMonths : Int -> List Month -> List Date
+    everyDateInMonths y ms =
+      List.concatMap (everyDateInMonth y) ms
   in
-    suite "RataDie" <|
-      List.map defaultTest <| assertionList ymdList <| List.map ymdFromRataDieFromYMD ymdList
+    suite "quarter" [
+      test "Q1" <| assert
+        <| (everyDateInMonths 2016 [Jan, Feb, Mar] |> List.map quarter |> List.all ((==) 1)),
+      test "Q2" <| assert
+        <| (everyDateInMonths 2016 [Apr, May, Jun] |> List.map quarter |> List.all ((==) 2)),
+      test "Q3" <| assert
+        <| (everyDateInMonths 2016 [Jul, Aug, Sep] |> List.map quarter |> List.all ((==) 3)),
+      test "Q4" <| assert
+        <| (everyDateInMonths 2016 [Oct, Nov, Dec] |> List.map quarter |> List.all ((==) 4))
+    ]
+
 
 tests : Test
 tests =
   suite "DateX" [
+    rataDieTests,
     isoWeekDateTests,
-    rataDieTests
+    quarterTests
   ]
 
 main : Program Never
