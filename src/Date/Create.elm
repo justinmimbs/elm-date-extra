aa02ddce2eea8b6f4be1634d7fe4fd9273b64139
Justin Mimbs <justin.mimbs@gmail.com>
2016-06-28 21:36:51 -0400

    Add documentation to Create and Extract

--- src/Date/Create.elm
+++ src/Date/Create.elm
@@ -3,11 +3,41 @@ module Date.Create exposing (
   fromCalendarDate,
   fromIsoString,
   TimeZone, utc, offset, local,
-  TimeSpec, noTime, timeAt,
+  TimeSpec, noTime, atTime,
   DateSpec, calendarDate, ordinalDate, weekDate,
   fromSpec,
   fromJulianDate
   )
+
+{-| A `Date` represents a moment in time, encoded by two essential pieces of
+information: the number of milliseconds since January 1, 1970 UTC, and the
+offset between UTC and your local time. Extractions of a `Date` (e.g.
+`Date.month`, `Date.hour`) return representations of the date in local time,
+while `Date.toTime` returns the UTC representation.
+
+# Common Constructors
+@docs fromParts, fromCalendarDate, fromIsoString
+
+# Detailed Specification
+In some cases you may want to specify a date with a time zone offset or from
+week-date or ordinal-date parts. The `fromSpec` function provides a way to
+do this.
+
+@docs fromSpec
+
+## TimeZone
+@docs TimeZone, utc, offset, local
+
+## TimeSpec
+@docs TimeSpec, noTime, atTime
+
+## DateSpec
+@docs DateSpec, calendarDate, weekDate, ordinalDate
+
+# Julian Dates
+@docs fromJulianDate
+-}
+
 
 import Date exposing (Date, Month)
 import Date.Facts exposing (msPerMinute, msPerDay)
@@ -34,77 +64,172 @@ fromOffsetTime (offset, time) =
         fromTime <| time - msPerMinute * localOffset
 
 
+{-| Create a `Date` from the following parts, given in local time:
+
+- year
+- month
+- day
+- hour
+- minute
+- second
+- millisecond
+
+
+    import Date exposing (Month(..))
+    import Date.Create as Date
+
+    -- 31 December 1999, 11:59 p.m., local time
+    date = Date.fromParts 1999 Dec 31 23 59 0 0
+
+Values of the number parts are not checked to ensure a valid date
+representation; providing values outside of a valid range results in underflow
+or overflow, rather than clamping.
+
+    -- 1 March 2007
+    date = Date.fromParts 2007 Feb 29 0 0 0 0
+-}
 fromParts : Int -> Month -> Int -> Int -> Int -> Int -> Int -> Date
 fromParts y m d hh mm ss ms =
   fromOffsetTime (Nothing, unixTimeFromParts y m d hh mm ss ms)
 
 
+{-| Convenience function for creating a `Date` from only the year, month, and
+day parts.
+
+    date = Date.fromCalendarDate 2000 Jan 1
+-}
 fromCalendarDate : Int -> Month -> Int -> Date
 fromCalendarDate y m d =
   fromOffsetTime (Nothing, unixTimeFromCalendarDate y m d)
 
 
+{-| Attempt to create a `Date` from a string formatted as a valid [ISO 8601](
+https://en.wikipedia.org/wiki/ISO_8601) date or date-time representation.
+
+    -- Just <1 January 2000, local time>
+    date = Date.fromIsoString "2000-01-01"
+
+    -- Just <5 August 2016, 23:00, UTC>
+    opening = Date.fromIsoString "2016-08-05T20:00-03:00"
+
+    -- Nothing
+    notIso = Date.fromIsoString "1/1/2001"
+
+When a `Date` is created with a specified time zone offset (e.g. `"-03:00"`),
+its extractions still represent local time, and `Date.toTime` still represents
+its UTC time.
+-}
 fromIsoString : String -> Maybe Date
 fromIsoString s =
   Maybe.map fromOffsetTime <| offsetTimeFromIsoString s
 
 
+{-| Represents a time zone.
+-}
 type TimeZone
   = Offset (Maybe Int)
 
 
+{-| UTC time zone.
+-}
 utc : TimeZone
 utc =
   Offset (Just 0)
 
 
+{-| Create a `TimeZone` with a specific offset from UTC time, given in minutes.
+-}
 offset : Int -> TimeZone
 offset minutes =
   Offset (Just minutes)
 
 
+{-| Do not specify a time zone, but use the local offset instead.
+-}
 local : TimeZone
 local =
   Offset Nothing
 
 
+{-| Represents a time of day.
+-}
 type TimeSpec
   = TimeMS Int
 
+
+{-| Do not specify a time of day (i.e. midnight).
+-}
 noTime : TimeSpec
 noTime =
   TimeMS 0
 
 
-timeAt : Int -> Int -> Int -> Int -> TimeSpec
-timeAt hh mm ss ms =
+{-| Create a `TimeSpec` from time parts (hour, minute, second, millisecond).
+-}
+atTime : Int -> Int -> Int -> Int -> TimeSpec
+atTime hh mm ss ms =
   TimeMS <| msFromTimeParts hh mm ss ms
 
 
+{-| Represents a day.
+-}
 type DateSpec
   = DateMS Int
 
 
+{-| Create a `DateSpec` from calendar-date parts (year, month, day).
+-}
 calendarDate : Int -> Month -> Int -> DateSpec
 calendarDate y m d =
   DateMS <| unixTimeFromCalendarDate y m d
 
 
+{-| Create a `DateSpec` from ordinal-date parts (year, ordinalDay).
+-}
 ordinalDate : Int -> Int -> DateSpec
 ordinalDate y d =
   DateMS <| unixTimeFromCalendarDate y Date.Jan d
 
 
+{-| Create a `DateSpec` from week-date parts (weekYear, weekNumber, weekdayNumber).
+-}
 weekDate : Int -> Int -> Int -> DateSpec
 weekDate y w d =
   DateMS <| unixTimeFromWeekDate y w d
 
 
+{-| Create a `Date` from a specified time zone, time of day, and day.
+
+    -- 1 January 2000, UTC
+    date = Date.fromSpec
+      utc
+      noTime
+      (calendarDate 2000 Jan 1)
+
+    -- 5 August 2016, 20:00, UTC-03:00
+    opening = Date.fromSpec
+      (offset -180)
+      (atTime 20 0 0 0)
+      (calendarDate 2016 Aug 5)
+
+    -- 29 December 2008, local time
+    week1 = Date.fromSpec
+      local
+      noTime
+      (weekDate 2009 1 1)
+
+When a `Date` is created with a specified time zone offset (e.g. `offset -180`),
+its extractions still represent local time, and `Date.toTime` still represents
+its UTC time.
+-}
 fromSpec : TimeZone -> TimeSpec -> DateSpec -> Date
 fromSpec (Offset o) (TimeMS t) (DateMS d) =
   fromOffsetTime (o, d + t)
 
 
+{-| Create a `Date` from a [Julian Date](https://en.wikipedia.org/wiki/Julian_day).
+Julian Dates always represent UTC time.
+-}
 fromJulianDate : Float -> Date
 fromJulianDate j =
   fromTime <| round <| (j - 2440587.5) * toFloat msPerDay
--- src/Date/Extract.elm
+++ src/Date/Extract.elm
@@ -9,26 +9,45 @@ module Date.Extract exposing (
   offsetFromUtc
   )
 
+{-| This module provides additional extractions to those available in the
+Core library.
+
+@docs monthNumber, quarter, ordinalDay, fractionalDay, weekdayNumber, weekNumber, weekYear, offsetFromUtc
+-}
+
+
 import Date exposing (Date, Month(..), toTime, year, month, day, hour, minute, second, millisecond, dayOfWeek)
 import Date.Facts exposing (monthNumberFromMonth, weekdayNumberFromDayOfWeek, daysBeforeStartOfMonth, msPerMinute, msPerDay)
 import Date.Internal.Core exposing (unixTimeFromParts, weekYearFromCalendarDate, weekNumberFromCalendarDate, msFromTimeParts)
 
 
+{-| Extract the month number of a date. Given the date 23 June 1990 at
+11:45 a.m. this returns the integer 6.
+-}
 monthNumber : Date -> Int
 monthNumber date =
   monthNumberFromMonth <| month date
 
 
+{-| Extract the quarter of a date. Given the date 23 June 1990 at
+11:45 a.m. this returns the integer 2.
+-}
 quarter : Date -> Int
 quarter date =
   monthNumber date |> toFloat |> (\n -> n / 3) |> ceiling
 
 
+{-| Extract the ordinal day of a date. Given the date 23 June 1990 at
+11:45 a.m. this returns the integer 174.
+-}
 ordinalDay : Date -> Int
 ordinalDay date =
   daysBeforeStartOfMonth (year date) (month date) + day date
 
 
+{-| Extract the fractional day of a date. Given the date 23 June 1990 at
+11:45 a.m. this returns the float 0.4895833333333333.
+-}
 fractionalDay : Date -> Float
 fractionalDay date =
   let
@@ -37,19 +56,28 @@ fractionalDay date =
     toFloat timeOfDayMS / toFloat msPerDay
 
 
-weekYear : Date -> Int
-weekYear date =
-  weekYearFromCalendarDate (year date) (month date) (day date)
+{-| Extract the weekday number (beginning at 1 for Monday) of a date. Given
+the date 23 June 1990 at 11:45 a.m. this returns the integer 6.
+-}
+weekdayNumber : Date -> Int
+weekdayNumber date =
+  weekdayNumberFromDayOfWeek <| dayOfWeek date
 
 
+{-| Extract the week number of a date. Given the date 23 June 1990 at
+11:45 a.m. this returns the integer 25.
+-}
 weekNumber : Date -> Int
 weekNumber date =
   weekNumberFromCalendarDate (year date) (month date) (day date)
 
 
-weekdayNumber : Date -> Int
-weekdayNumber date =
-  weekdayNumberFromDayOfWeek <| dayOfWeek date
+{-| Extract the week-numbering year of a date. Given the date 23 June
+1990 at 11:45 a.m. this returns the integer 1990.
+-}
+weekYear : Date -> Int
+weekYear date =
+  weekYearFromCalendarDate (year date) (month date) (day date)
 
 
 msOffsetFromUtc : Date -> Int
@@ -61,6 +89,8 @@ msOffsetFromUtc date =
     localTime - utcTime |> floor
 
 
+{-| Extract the offset from UTC time in minutes of a date. Given a date with a local offset of UTC-05:00 this returns the integer -300.
+-}
 offsetFromUtc : Date -> Int
 offsetFromUtc date =
   msOffsetFromUtc date // msPerMinute
