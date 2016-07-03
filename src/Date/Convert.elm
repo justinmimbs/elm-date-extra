34e3be7c562a74cedf3c357ba8b4d15c5d564000
Justin Mimbs <justin.mimbs@gmail.com>
2016-07-02 22:23:16 -0400

    Update toFormattedString to use UTS 35 Date Format Patterns; add documentation to Convert

--- src/Date/Convert.elm
+++ src/Date/Convert.elm
@@ -22,48 +22,101 @@ import Date.Internal.Core exposing (unixTimeFromParts)
 import Date.Internal.Format
 
 
-{-|
+{-| Convert a date to a string using a pattern as a template.
+
+    Date.toFormattedString
+      "EEEE, MMMM d, y 'at' h:mm a"
+      (Date.fromParts 2007 Mar 15 13 45 56 67)
+
+    -- "Thursday, March 15, 2007 at 1:45 PM"
+
+Each alphabetic character in the pattern represents date or time information;
+the number of times a character is repeated specifies the form of the name to
+use (e.g. "Tue", "Tuesday") or the padding of numbers (e.g. "1", "01").
+Formatting characters are escaped within single-quotes, and a single-quote is
+escaped as a sequence of two single-quotes, whether appearing inside or outside
+an escaped sequence.
+
+Patterns are based on Date Format Patterns in [Unicode Technical
+Standard #35](http://www.unicode.org/reports/tr35/tr35-43/
+tr35-dates.html#Date_Format_Patterns). Only the following subset of
+formatting characters are available:
+
+    'y' -- year
+    'Y' -- week-numbering year
+    'Q' -- quarter
+    'M' -- month
+    'w' -- week number
+    'd' -- day
+    'D' -- ordinal day
+    'E' -- day of week
+    'e' -- weekday number / day of week
+    'a' -- day period
+    'b' -- day period
+    'h' -- hour (12-hour clock)
+    'H' -- hour (24-hour clock)
+    'm' -- minute
+    's' -- second
+    'S' -- fractional second
+    'X' -- time offset, using "Z" when offset is 0
+    'x' -- time offset
+
+The non-standard pattern field "ddd" is available to indicate the day of the
+month with an ordinal suffix (e.g. "1st", "15th"), as the current standard does
+not include such a field.
+
+    Date.toFormattedString
+      "MMMM ddd, y"
+      (Date.fromParts 2007 Mar 15 13 45 56 67)
+
+    -- "March 15th, 2007"
 -}
 toFormattedString : String -> Date -> String
 toFormattedString =
   Date.Internal.Format.toFormattedString False
 
 
-{-| Convenience function for formatting a date to ISO 8601 extended date-time
-format with local time zone offset.
+{-| Convenience function for formatting a date to ISO 8601 (extended
+date and time format with local time offset).
 
-    -- assuming a local offset of UTC-04:00
+    Date.toTimestamp (Date.fromParts 2007 Mar 15 13 45 56 67)
 
-    Date.toTimestamp (Date.fromParts 2007 Mar 15 11 55 0 0)
-    -- "2007-03-15T11:55:00.000-04:00"
+    -- "2007-03-15T13:45:56.067-04:00"
+    -- (example has a local offset of UTC-04:00)
 -}
 toTimestamp: Date -> String
 toTimestamp =
-  toFormattedString "yyyy-mm-ddTHH:MM:ss.lP"
+  toFormattedString "yyyy-MM-dd'T'HH:mm:ss.SSSxxx"
 
 
-{-|
+{-| Convert a date to a string just like `toFormattedString`, but using the UTC
+representation instead of the local representation of the date.
 -}
 toUtcFormattedString : String -> Date -> String
 toUtcFormattedString =
   Date.Internal.Format.toFormattedString True
 
 
-{-| Convenience function for formatting a date to ISO 8601 extended date-time
-format as UTC time.
+{-| Convenience function for formatting a date, in UTC representation, to ISO
+8601 (extended date and time format with "Z" for time offset).
 
-    -- assuming a local offset of UTC-04:00
+    Date.toUtcTimestamp (Date.fromParts 2007 Mar 15 13 45 56 67)
 
-    Date.toTimestamp (Date.fromParts 2007 Mar 15 11 55 0 0)
-    -- "2007-03-15T15:55:00.000Z"
+    -- "2007-03-15T17:45:56.067Z"
+    -- (example has a local offset of UTC-04:00)
 -}
 toUtcTimestamp: Date -> String
 toUtcTimestamp =
-  toUtcFormattedString "yyyy-mm-ddTHH:MM:ss.lZ"
+  toUtcFormattedString "yyyy-MM-dd'T'HH:mm:ss.SSSXXX"
 
 
-{-| Convert a `Date` to a [Julian Date](https://en.wikipedia.org/wiki/Julian_day).
+{-| Convert a date to a [Julian Date](https://en.wikipedia.org/wiki/Julian_day).
 Julian Dates always represent UTC time.
+
+    Date.toJulianDate (Date.fromSpec utc noTime (calendarDate 2000 Jan 1))
+
+    -- 2451544.5
+    -- (example has a local offset of UTC+00:00)
 -}
 toJulianDate : Date -> Float
 toJulianDate date =
--- src/Date/Internal/Format.elm
+++ src/Date/Internal/Format.elm
@@ -5,7 +5,7 @@ module Date.Internal.Format exposing (
 import Date exposing (Date, Month(..), Day(..), year, month, day, hour, minute, second, millisecond, dayOfWeek)
 import Date.Facts exposing (msPerMinute)
 import Date.Extract exposing (monthNumber, quarter, ordinalDay, weekYear, weekNumber, weekdayNumber, offsetFromUtc)
-import String exposing (slice, padLeft)
+import String exposing (slice, left, right, padLeft, padRight, toUpper, toLower)
 import Regex exposing (Regex, regex, replace, HowMany(..))
 
 
@@ -45,86 +45,230 @@ hour12 date =
     h -> h
 
 
+type DayPeriod
+  = Midnight
+  | AM
+  | Noon
+  | PM
+
+
+dayPeriod : Date -> DayPeriod
+dayPeriod date =
+  let
+    hh = hour date
+    onTheHour = minute date == 0 && second date == 0 && millisecond date == 0
+  in
+    if hh == 0 && onTheHour then
+      Midnight
+    else if hh < 12 then
+      AM
+    else if hh == 12 && onTheHour then
+      Noon
+    else
+      PM
+
+
 ordinalSuffix : Int -> String
 ordinalSuffix n =
-    let
-      -- use 2-digit number
-      nn = n % 100
-    in
-      case min (if nn < 20 then nn else nn % 10) 4 of
-        0 -> "th"
-        1 -> "st"
-        2 -> "nd"
-        3 -> "rd"
-        4 -> "th"
-        _ -> ""
-
-
-formatTimeZoneOffset : String -> Int -> String
-formatTimeZoneOffset separator minutes =
   let
-    hh = abs minutes // 60 |> toString |> padLeft 2 '0'
-    mm = abs minutes % 60 |> toString |> padLeft 2 '0'
-    sign = if minutes >= 0 then "+" else "-"
+    -- use 2-digit number
+    nn = n % 100
   in
-    sign ++ hh ++ separator ++ mm
+    case min (if nn < 20 then nn else nn % 10) 4 of
+      0 -> "th"
+      1 -> "st"
+      2 -> "nd"
+      3 -> "rd"
+      4 -> "th"
+      _ -> ""
 
 
-tokens : Regex
-tokens =
-  regex "yy(?:yy)?|m{1,4}|d{1,4}|D(?:DD)?|([wHhMsAa])\\1?|[SqNolOP]|\\[.*?\\]"
+withOrdinalSuffix : Int -> String
+withOrdinalSuffix n =
+  toString n ++ ordinalSuffix n
+
+
+formatTimeOffset : String -> Bool -> Int -> String
+formatTimeOffset separator minutesOptional offset =
+  let
+    sign = if offset >= 0 then "+" else "-"
+    hh = abs offset // 60 |> toString |> padLeft 2 '0'
+    mm = abs offset % 60 |> toString |> padLeft 2 '0'
+  in
+    if minutesOptional && mm == "00" then
+      sign ++ hh
+    else
+      sign ++ hh ++ separator ++ mm
+
+
+-- Formatting is based on Date Format Patterns in Unicode Technical Standard #35
+
+
+{- Matches a series of pattern characters, or a single-quoted string (which
+may contain '' inside, representing an escaped single-quote).
+-}
+patternMatches : Regex
+patternMatches =
+  regex "([yYQMwdDEeabhHmsSXx])\\1*|'(?:[^']|'')*?'(?!')"
+
+
+nameForm : Int -> String
+nameForm length =
+  case length of
+    1 -> "abbreviated"
+    2 -> "abbreviated"
+    3 -> "abbreviated"
+    4 -> "full"
+    5 -> "narrow"
+    6 -> "short"
+    _ -> "invalid"
 
 
 f : Bool -> Date -> String -> String
-f asUtc date token =
-  case token of
-    -- date
-    "yyyy" -> year date |> toString
-    "yy"   -> year date |> toString |> slice 2 4
-    "mmmm" -> month date |> monthName
-    "mmm"  -> month date |> monthName |> slice 0 3
-    "mm"   -> monthNumber date |> toString |> padLeft 2 '0'
-    "m"    -> monthNumber date |> toString
-    "dddd" -> dayOfWeek date |> dayOfWeekName
-    "ddd"  -> dayOfWeek date |> dayOfWeekName |> slice 0 3
-    "dd"   -> day date |> toString |> padLeft 2 '0'
-    "d"    -> day date |> toString
-    "D"    -> ordinalDay date |> toString
-    "DDD"  -> ordinalDay date |> toString |> padLeft 3 '0'
-    "S"    -> day date |> ordinalSuffix
-    "q"    -> quarter date |> toString
-    "o"    -> weekYear date |> toString
-    "ww"   -> weekNumber date |> toString |> padLeft 2 '0'
-    "w"    -> weekNumber date |> toString
-    "N"    -> weekdayNumber date |> toString
-    -- time
-    "HH"   -> hour date |> toString |> padLeft 2 '0'
-    "H"    -> hour date |> toString
-    "hh"   -> hour12 date |> toString |> padLeft 2 '0'
-    "h"    -> hour12 date |> toString
-    "MM"   -> minute date |> toString |> padLeft 2 '0'
-    "M"    -> minute date |> toString
-    "ss"   -> second date |> toString |> padLeft 2 '0'
-    "s"    -> second date |> toString
-    "l"    -> millisecond date |> toString |> padLeft 3 '0'
-    "AA"   -> if hour date < 12 then "AM" else "PM"
-    "A"    -> if hour date < 12 then "A" else "P"
-    "aa"   -> if hour date < 12 then "am" else "pm"
-    "a"    -> if hour date < 12 then "a" else "p"
-    "O"    -> (if asUtc then 0 else offsetFromUtc date) |> formatTimeZoneOffset ""
-    "P"    -> (if asUtc then 0 else offsetFromUtc date) |> formatTimeZoneOffset ":"
-    -- escaped
-    s      -> slice 1 -1 s
+f asUtc date match =
+  let
+    char = left 1 match
+    length = String.length match
+  in
+    case char of
+      "y" ->
+        case length of
+          2 -> year date |> toString |> padLeft length '0' |> right 2
+          _ -> year date |> toString |> padLeft length '0'
+      "Y" ->
+        case length of
+          2 -> weekYear date |> toString |> padLeft length '0' |> right 2
+          _ -> weekYear date |> toString |> padLeft length '0'
+      "Q" ->
+        case length of
+          1 -> quarter date |> toString
+          2 -> quarter date |> toString
+          3 -> quarter date |> toString |> (++) "Q"
+          4 -> quarter date |> withOrdinalSuffix
+          5 -> quarter date |> toString
+          _ -> ""
+      "M" ->
+        case length of
+          1 -> monthNumber date |> toString
+          2 -> monthNumber date |> toString |> padLeft 2 '0'
+          3 -> month date |> monthName |> left 3
+          4 -> month date |> monthName
+          5 -> month date |> monthName |> left 1
+          _ -> ""
+      "w" ->
+        case length of
+          1 -> weekNumber date |> toString
+          2 -> weekNumber date |> toString |> padLeft 2 '0'
+          _ -> ""
+      "d" ->
+        case length of
+          1 -> day date |> toString
+          2 -> day date |> toString |> padLeft 2 '0'
+          3 -> day date |> withOrdinalSuffix -- non-standard
+          _ -> ""
+      "D" ->
+        case length of
+          1 -> ordinalDay date |> toString
+          2 -> ordinalDay date |> toString |> padLeft 2 '0'
+          3 -> ordinalDay date |> toString |> padLeft 3 '0'
+          _ -> ""
+      "E" ->
+        case nameForm length of
+          "abbreviated" -> dayOfWeek date |> dayOfWeekName |> left 3
+          "full"        -> dayOfWeek date |> dayOfWeekName
+          "narrow"      -> dayOfWeek date |> dayOfWeekName |> left 1
+          "short"       -> dayOfWeek date |> dayOfWeekName |> left 2
+          _             -> ""
+      "e" ->
+        case length of
+          1 -> weekdayNumber date |> toString
+          2 -> weekdayNumber date |> toString
+          _ -> f asUtc date (toUpper match)
+      "a" ->
+        let
+          p = dayPeriod date
+          m = if p == Midnight || p == AM then "A" else "P"
+        in
+          case nameForm length of
+            "abbreviated"  -> m ++ "M"
+            "full"         -> m ++ ".M."
+            "narrow"       -> m
+            _              -> ""
+      "b" ->
+        case nameForm length of
+          "abbreviated" ->
+            case dayPeriod date of
+              Midnight -> "mid."
+              AM       -> "am"
+              Noon     -> "noon"
+              PM       -> "pm"
+          "full" ->
+            case dayPeriod date of
+              Midnight -> "midnight"
+              AM       -> "a.m."
+              Noon     -> "noon"
+              PM       -> "p.m."
+          "narrow" ->
+            case dayPeriod date of
+              Midnight -> "md"
+              AM       -> "a"
+              Noon     -> "nn"
+              PM       -> "p"
+          _ ->
+            ""
+      "h" ->
+        case length of
+          1 -> hour12 date |> toString
+          2 -> hour12 date |> toString |> padLeft 2 '0'
+          _ -> ""
+      "H" ->
+        case length of
+          1 -> hour date |> toString
+          2 -> hour date |> toString |> padLeft 2 '0'
+          _ -> ""
+      "m" ->
+        case length of
+          1 -> minute date |> toString
+          2 -> minute date |> toString |> padLeft 2 '0'
+          _ -> ""
+      "s" ->
+        case length of
+          1 -> second date |> toString
+          2 -> second date |> toString |> padLeft 2 '0'
+          _ -> ""
+      "S" ->
+        millisecond date |> toString |> padLeft 3 '0' |> left length |> padRight length '0'
+      "X" ->
+        if length < 4 && (asUtc || offsetFromUtc date == 0) then
+          "Z"
+        else
+          f asUtc date (toLower match)
+      "x" ->
+        let
+          offset = if asUtc then 0 else offsetFromUtc date
+        in
+          case length of
+            1 -> formatTimeOffset "" True offset
+            2 -> formatTimeOffset "" False offset
+            3 -> formatTimeOffset ":" False offset
+            _ -> ""
+      "'" ->
+        if match == "''" then
+          "'"
+        else
+          slice 1 -1 match |> replace All (regex "''") (\_ -> "'")
+      _ ->
+        ""
 
 
 toUtc : Date -> Date
 toUtc date =
-  Date.fromTime <| Date.toTime date - (toFloat (offsetFromUtc date * msPerMinute))
+  Date.fromTime <| Date.toTime date - (toFloat <| offsetFromUtc date * msPerMinute)
 
 
 toFormattedString : Bool -> String -> Date -> String
-toFormattedString asUtc format date =
+toFormattedString asUtc pattern date =
   let
     date' = if asUtc then toUtc date else date
   in
-    replace All tokens (\{ match } -> f asUtc date' match) format
+    replace All patternMatches (\{ match } -> f asUtc date' match) pattern
