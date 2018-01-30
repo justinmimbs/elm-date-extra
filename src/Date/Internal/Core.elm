783d6f164fb4c4f007520b5fda98de1109c71db0
Justin Mimbs <justin.mimbs@gmail.com>
2018-01-30 13:28:39 -0500

    Refactor internal RataDie module

--- src/Date/Internal/Core.elm
+++ src/Date/Internal/Core.elm
@@ -11,39 +11,10 @@ module Date.Internal.Core
 
 import Date exposing (Month)
 import Date.Extra.Facts exposing (daysBeforeStartOfMonth, months, msPerDay, msPerHour, msPerMinute, msPerSecond)
-import Date.Internal.RataDie as RataDie
+import Date.Internal.RataDie as RataDie exposing (RataDie)
 
 
-msFromTimeParts : Int -> Int -> Int -> Int -> Int
-msFromTimeParts hh mm ss ms =
-    ms
-        + msPerSecond
-        * ss
-        + msPerMinute
-        * mm
-        + msPerHour
-        * hh
-
-
-unixTimeFromParts : Int -> Month -> Int -> Int -> Int -> Int -> Int -> Int
-unixTimeFromParts y m d hh mm ss ms =
-    RataDie.toUnixTime (RataDie.fromCalendarDate y m d)
-        + msFromTimeParts hh mm ss ms
-
-
-unixTimeFromCalendarDate : Int -> Month -> Int -> Int
-unixTimeFromCalendarDate y m d =
-    RataDie.toUnixTime <| RataDie.fromCalendarDate y m d
-
-
-unixTimeFromWeekDate : Int -> Int -> Int -> Int
-unixTimeFromWeekDate y w d =
-    RataDie.toUnixTime <| RataDie.fromWeekDate y w d
-
-
-unixTimeFromOrdinalDate : Int -> Int -> Int
-unixTimeFromOrdinalDate y d =
-    RataDie.toUnixTime <| RataDie.fromOrdinalDate y d
+-- week numbering
 
 
 weekYearFromCalendarDate : Int -> Month -> Int -> Int
@@ -54,3 +25,38 @@ weekYearFromCalendarDate y m d =
 weekNumberFromCalendarDate : Int -> Month -> Int -> Int
 weekNumberFromCalendarDate y m d =
     RataDie.weekNumber <| RataDie.fromCalendarDate y m d
+
+
+
+-- unix time
+
+
+unixTimeFromRataDie : RataDie -> Int
+unixTimeFromRataDie rd =
+    (rd - 719163) * msPerDay
+
+
+unixTimeFromOrdinalDate : Int -> Int -> Int
+unixTimeFromOrdinalDate y d =
+    unixTimeFromRataDie <| RataDie.fromOrdinalDate y d
+
+
+unixTimeFromCalendarDate : Int -> Month -> Int -> Int
+unixTimeFromCalendarDate y m d =
+    unixTimeFromRataDie <| RataDie.fromCalendarDate y m d
+
+
+unixTimeFromWeekDate : Int -> Int -> Int -> Int
+unixTimeFromWeekDate y w d =
+    unixTimeFromRataDie <| RataDie.fromWeekDate y w d
+
+
+msFromTimeParts : Int -> Int -> Int -> Int -> Int
+msFromTimeParts hh mm ss ms =
+    msPerHour * hh + msPerMinute * mm + msPerSecond * ss + ms
+
+
+unixTimeFromParts : Int -> Month -> Int -> Int -> Int -> Int -> Int -> Int
+unixTimeFromParts y m d hh mm ss ms =
+    unixTimeFromCalendarDate y m d
+        + msFromTimeParts hh mm ss ms
--- src/Date/Internal/RataDie.elm
+++ src/Date/Internal/RataDie.elm
@@ -1,155 +1,65 @@
 module Date.Internal.RataDie
     exposing
-        ( day
+        ( RataDie
         , fromCalendarDate
         , fromOrdinalDate
         , fromWeekDate
-        , month
-        , ordinalDay
-        , toUnixTime
         , weekNumber
         , weekYear
-        , weekdayNumber
-        , year
         )
 
 import Date exposing (Month)
-import Date.Extra.Facts exposing (daysBeforeStartOfMonth, months, msPerDay)
-
-
-find : (a -> Bool) -> List a -> Maybe a
-find pred list =
-    case list of
-        [] ->
-            Nothing
-
-        x :: xs ->
-            if pred x then
-                Just x
-            else
-                find pred xs
-
-
-
--- integer division returning (Quotient, Remainder)
-
-
-divideInt : Int -> Int -> ( Int, Int )
-divideInt a b =
-    ( a // b, rem a b )
-
-
-
--- RataDie
+import Date.Extra.Facts exposing (daysBeforeStartOfMonth, isLeapYear)
 
 
 type alias RataDie =
     Int
 
 
-leapYearsInCommonEra : Int -> Int
-leapYearsInCommonEra y =
-    (y // 4) - (y // 100) + (y // 400)
-
-
-rataDieBeforeStartOfYear : Int -> RataDie
-rataDieBeforeStartOfYear y =
-    365 * (y - 1) + leapYearsInCommonEra (y - 1)
-
-
-
--- Create
-
-
 fromOrdinalDate : Int -> Int -> RataDie
-fromOrdinalDate y d =
-    rataDieBeforeStartOfYear y + d
+fromOrdinalDate y od =
+    daysBeforeYear y + od
 
 
 fromCalendarDate : Int -> Month -> Int -> RataDie
 fromCalendarDate y m d =
-    let
-        yd =
-            rataDieBeforeStartOfYear y
-
-        md =
-            daysBeforeStartOfMonth y m
-    in
-    yd + md + d
+    daysBeforeYear y + daysBeforeStartOfMonth y m + d
 
 
 fromWeekDate : Int -> Int -> Int -> RataDie
-fromWeekDate y w d =
+fromWeekDate wy wn wdn =
+    daysBeforeWeekYear wy + (wn - 1) * 7 + wdn
+
+
+weekYear : RataDie -> Int
+weekYear rd =
+    -- `year <thursday of this week>`
+    year (rd + (4 - weekdayNumber rd))
+
+
+weekNumber : RataDie -> Int
+weekNumber rd =
     let
-        week1Day0RD =
-            week1Day1OfWeekYear y - 1
+        week1Day1 =
+            daysBeforeWeekYear (weekYear rd) + 1
     in
-    week1Day0RD + (w - 1) * 7 + d
+    (rd - week1Day1) // 7 + 1
 
 
 
--- Extract
+-- calculations
 
 
-year : RataDie -> Int
-year rd =
-    let
-        ( q400, r400 ) =
-            divideInt rd 146097
-
-        -- 400 * 365 + 97
-        ( q100, r100 ) =
-            divideInt r400 36524
-
-        -- 100 * 365 + 24
-        ( q4, r4 ) =
-            divideInt r100 1461
-
-        --   4 * 365 +  1
-        ( q1, r1 ) =
-            divideInt r4 365
-
-        n =
-            if r1 == 0 then
-                0
-            else
-                1
-    in
-    q400 * 400 + q100 * 100 + q4 * 4 + q1 + n
-
-
-ordinalDay : RataDie -> Int
-ordinalDay rd =
-    rd - rataDieBeforeStartOfYear (year rd)
-
-
-month : RataDie -> Month
-month rd =
+daysBeforeYear : Int -> Int
+daysBeforeYear y1 =
     let
         y =
-            year rd
+            y1 - 1
 
-        od =
-            ordinalDay rd
+        leapYears =
+            (y // 4) - (y // 100) + (y // 400)
     in
-    List.reverse months
-        |> find (\m -> daysBeforeStartOfMonth y m < od)
-        |> Maybe.withDefault Date.Jan
-
-
-day : RataDie -> Int
-day rd =
-    let
-        y =
-            year rd
-
-        m =
-            month rd
-
-        od =
-            ordinalDay rd
-    in
-    od - daysBeforeStartOfMonth y m
+    365 * y + leapYears
 
 
 weekdayNumber : RataDie -> Int
@@ -162,37 +72,44 @@ weekdayNumber rd =
             n
 
 
-weekYear : RataDie -> Int
-weekYear rd =
+daysBeforeWeekYear : Int -> Int
+daysBeforeWeekYear y =
     let
-        daysToThursday =
-            4 - weekdayNumber rd
+        jan4 =
+            daysBeforeYear y + 4
     in
-    year (rd + daysToThursday)
+    jan4 - weekdayNumber jan4
 
 
-week1Day1OfWeekYear : Int -> RataDie
-week1Day1OfWeekYear y =
+year : RataDie -> Int
+year rd =
     let
-        jan4RD =
-            fromOrdinalDate y 4
+        ( n400, r400 ) =
+            -- 400 * 365 + 97
+            divideInt rd 146097
+
+        ( n100, r100 ) =
+            -- 100 * 365 + 24
+            divideInt r400 36524
+
+        ( n4, r4 ) =
+            -- 4 * 365 + 1
+            divideInt r100 1461
+
+        ( n1, r1 ) =
+            divideInt r4 365
+
+        n =
+            if r1 == 0 then
+                0
+            else
+                1
     in
-    jan4RD - weekdayNumber jan4RD + 1
+    n400 * 400 + n100 * 100 + n4 * 4 + n1 + n
 
 
-weekNumber : RataDie -> Int
-weekNumber rd =
-    let
-        week1Day1RD =
-            week1Day1OfWeekYear (weekYear rd)
-    in
-    (rd - week1Day1RD) // 7 + 1
-
-
-
--- Convert
-
-
-toUnixTime : RataDie -> Int
-toUnixTime rd =
-    (rd - 719163) * msPerDay
+{-| integer division, returning (Quotient, Remainder)
+-}
+divideInt : Int -> Int -> ( Int, Int )
+divideInt a b =
+    ( a // b, rem a b )
